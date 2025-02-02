167eec7b72a15b5ad1b059cd33b858881959bffd
SharminMily <sharminmily@gmail.com>
2025-02-02 20:07:42 +0600

    RTK Query setup

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,14 @@ import { PrimeReactProvider } from 'primereact/api';
 
 import "primereact/resources/themes/lara-light-cyan/theme.css";
 import 'primeicons/primeicons.css';
+import { Provider } from 'react-redux';
+import { store } from './redux/store.ts';
 
 createRoot(document.getElementById('root')!).render(
   <div className="max-w-7xl mx-auto bg-[#010113]">
   <PrimeReactProvider>
-  <StrictMode>  
+  <StrictMode> 
+    <Provider store={store}></Provider> 
     <RouterProvider router={router} />
   </StrictMode>
    </PrimeReactProvider>
--- src/redux/api/product.ts
+++ src/redux/api/product.ts
@@ -1,18 +1,38 @@
-// Need to use the React-specific entry point to import createApi
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import type { Pokemon } from './types'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // Define a service using a base URL and expected endpoints
-export const pokemonApi = createApi({
-  reducerPath: 'pokemonApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://pokeapi.co/api/v2/' }),
+export const quizApi = createApi({
+  reducerPath: "products",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api" }),
+  tagTypes: ["product"],
   endpoints: (builder) => ({
-    getPokemonByName: builder.query<Pokemon, string>({
-      query: (name) => `pokemon/${name}`,
+    getAllProducts: builder.query({
+      query: () => `/products`,
+      providesTags: ["product"],
+    }),
+    addProducts: builder.mutation({
+      query: (body) => ({
+    
+        url: `/products`,
+        method: "POST",
+        body,
+      }),
+      invalidatesTags: ["product"],
+    }),
+    updateProducts: builder.mutation({
+      query: ({id,data},) => ({
+        url: `/products/${id}`,
+        method: "PATCH",
+        data,
+      }),
+    }),
+    deleteProducts: builder.mutation({
+      query: (id) => ({
+        url: `/products/${id}`,
+        method: "DELETE",
+      }),
     }),
   }),
-})
+});
 
-// Export hooks for usage in functional components, which are
-// auto-generated based on the defined endpoints
-export const { useGetPokemonByNameQuery } = pokemonApi
+export const { useGetAllQuizQuery,useAddQuizMutation } = quizApi;
